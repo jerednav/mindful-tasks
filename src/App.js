852efc91c48adb653348ad753f463c4e7e88a505
jerednav <80506833+jerednav@users.noreply.github.com>
2022-01-07 16:08:38 -0800

    fetching user data

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signup from "./pages/Signup/Signup";
 import Project from "./pages/Project/Project";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
   const { authIsReady, user } = useAuthContext();
@@ -34,6 +35,7 @@ function App() {
               />
             </Routes>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
