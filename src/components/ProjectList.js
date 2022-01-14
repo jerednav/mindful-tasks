c632e15dab563a4d11947d858105011580daf77c
jerednav <80506833+jerednav@users.noreply.github.com>
2022-01-14 14:10:50 -0800

    fetch projects to dashboard

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,6 +1,14 @@
 //styles
+import { projectAuth } from "../firebase/config";
 import "./projectlist.css";
 
 export default function ProjectList({ projects }) {
-  return <div></div>;
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet.</p>}
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  );
 }
