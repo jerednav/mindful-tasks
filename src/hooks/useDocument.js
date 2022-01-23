47ee0bef85cca4d2344f46394e57f58031e50fff
jerednav <80506833+jerednav@users.noreply.github.com>
2022-01-22 18:06:46 -0800

    Creating during successfulannp

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,4 +1,28 @@
 import { useEffect, useState } from "react";
 import { projectFireStore } from "../firebase/config";
 
-export const useDocument = (collection) => {};
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  //realtime data for document
+  useEffect =
+    (() => {
+      const ref = projectFireStore.collection(collection).doc(id);
+
+      const unsubscribe = ref.onSnapshot(
+        (snapshot) => {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        },
+        (err) => {
+          console.log(err.message);
+          setError("Failed to get document");
+        }
+      );
+      return () => unsubscribe();
+    },
+    [collection, id]);
+
+  return { document, error };
+};
