eacaa1e9a81b136bbf9a082b93c2e6a3679b527a
jerednav <80506833+jerednav@users.noreply.github.com>
2022-01-27 23:21:28 -0800

    Cleaning up filter components in dashboard and project filter

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ProjectList from "../../components/ProjectList";
 import { useCollection } from "../../hooks/useCollection";
 
@@ -7,12 +8,22 @@ import ProjectFilter from "./ProjectFilter";
 
 export default function Dashboard() {
   const { documents, error } = useCollection("projects");
+  const [currentFilter, setCurrentFilter] = useState("all");
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
 
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
-      {documents && <ProjectFilter />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
       {documents && <ProjectList projects={documents} />}
     </div>
   );
