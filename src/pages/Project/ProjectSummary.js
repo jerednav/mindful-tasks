0abb10ac407f26bd9bfb6416d2fc08af4c3ee57f
jerednav <80506833+jerednav@users.noreply.github.com>
2022-01-27 20:44:43 -0800

    completing (deleting) projects)

--- src/pages/Project/ProjectSummary.js
+++ src/pages/Project/ProjectSummary.js
@@ -1,21 +1,41 @@
 import Avatar from "../../components/Avatar";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    deleteDocument(project.id);
+    navigate("/");
+  };
+
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By: {project.createdBy.displayName}</p>
         <p className='due-date'>
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
         <p className='details'>{project.details}</p>
         <h4>Project is assigned to:</h4>
-        {project.assignedUsersList.map((user) => (
-          <div key={user.id}>
-            <Avatar src={user.photoURL} />
-          </div>
-        ))}
+        <div className='assigned-users'>
+          {project.assignedUsersList.map((user) => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
+        </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button class='btn' onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
